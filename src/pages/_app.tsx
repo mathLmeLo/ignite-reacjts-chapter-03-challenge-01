a3277a183cfc8a8d54633184a658fa632f372c54
Matheus Melo <matheus.correia@lavid.ufpb.br>
2023-04-11 02:21:12 -0300

    first commit: prismic custom type with slice machine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.scss';
 
+import Link from 'next/link';
+import { PrismicProvider } from '@prismicio/react';
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '../../prismicio';
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <PrismicProvider internalLinkComponent={props => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  );
 }
 
 export default MyApp;
